57fc4d9cfecaa565869ead9ead76f1e3107d3bac
RhysWareham <rhys_wareham@hotmail.co.uk>
2021-11-11 03:50:13 +0000

    Basic Menu Switching working

---
+++ CharacterCreator/Assets/BodyPart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+
+    public enum IndividualBodyPart
+    {
+        HEAD,
+        SHIRT,
+        ARMS,
+        LEGS,
+        SHOES
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CharacterCreator/Assets/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private enum MenuPages
+    {
+        HEAD,
+        SHIRT,
+        ARMS,
+        LEGS,
+        SHOES
+    }
+
+    [SerializeField]
+    private GameObject[] bodyPartCollection;
+
+    private MenuPages currentMenuPage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayBodyParts(int bodyPartType)
+    {
+        //
+        if(bodyPartType != (int)currentMenuPage)
+        {
+            bodyPartCollection[(int)currentMenuPage].SetActive(false);
+            bodyPartCollection[bodyPartType].SetActive(true);
+            currentMenuPage = (MenuPages)bodyPartType;
+        }
+    }
+}
